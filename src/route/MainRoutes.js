24caa831eaf1eeb76c285ccaf42987a4b4926fad
Anandhu5646 <anandhus05646@gmail.com>
2023-09-22 23:23:27 +0530

    interceptors added

--- src/route/MainRoutes.js
+++ src/route/MainRoutes.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Feed from "../pages/Feed";
 import Library from "../pages/Library";
@@ -8,21 +8,39 @@ import Favourite from "../pages/Favourite";
 import './Route.css'
 import Sidebar from "../components/Sidebar";
 import Login from "../components/auth/Login.js";
+import { setClietToken } from "../spotify";
 
 
 export default function MainRoutes() {
-  return (
+  const [token, setToken]=useState("")
+
+  useEffect(()=>{
+    const token=window.localStorage.getItem("token")
+    const hash=window.location.hash
+    window.location.hash("")
+    if(!token && hash){
+
+      const _token=hash.split("&")[0].split("=")[1]
+      window.localStorage.setItem("token",_token)
+      setToken(_token)
+      setClietToken(_token)
+    }else{
+      setToken(token)
+      setClietToken(token)
+    }
+  },[])
+  return (!token ?
+      <Login/>:
     <Router>
       <div className="main-body">
-      <Login/>
-        {/* <Sidebar/>
+        <Sidebar/>
       <Routes>
         <Route path="/" element={<Library />} />
         <Route path="/feed" element={<Feed />} />
         <Route path="/trending" element={<Trending />} />
         <Route path="/player" element={<Player />} />
         <Route path="/favourite" element={<Favourite />} />
-      </Routes> */}
+      </Routes>
       </div>
     </Router>
   );
--- src/spotify.js
+++ src/spotify.js
@@ -1,3 +1,5 @@
+import axios from "axios"
+
 const authEndPoint="https://accounts.spotify.com/authorize?"
 const clientId="e0e0a10f002340fb91379dac1c5521ae"
 const redirectUrl="http://localhost:3000"
@@ -5,3 +7,16 @@ const scope=["user-library-read","playlist-read-private"]
 
 export const loginEndPoint=`${authEndPoint}client_id=${clientId}&redirect_uri=${redirectUrl}
 &scope=${scope.join("%20")}&response_type=token&show_dialog=true` 
+
+const apiClient=axios.create({
+    baseURL:"https://api.spotify.com/v1/"
+})
+
+export const setClietToken=(token)=>{
+    apiClient.interceptors.request.use(async function (config){
+        config.headers.Authorization="Bearer" +token
+        return config
+    })
+}
+
+export default apiClient
